87ee106e05928f89f9d1199a797acf66923d4b75
max397574 <max.heinzer397@gmx.ch>
2021-06-07 17:58:37 +0200

    added challenge

---
+++ parse_molecule.py
@@ -0,0 +1,28 @@
+#https://www.codewars.com/kata/52f831fa9d332c6591000511
+from collections import Counter
+import re
+
+COMPONENT_RE = (
+    r'('
+        r'[A-Z][a-z]?'
+        r'|'
+        r'\([^(]+\)'
+        r'|'
+        r'\[[^[]+\]'
+        r'|'
+        r'\{[^}]+\}'
+    r')'
+    r'(\d*)'
+)
+
+
+def parse_molecule(formula):
+    counts = Counter()
+    for element, count in re.findall(COMPONENT_RE, formula):
+        count = int(count) if count else 1
+        if element[0] in '([{':
+            for k, v in parse_molecule(element[1:-1]).items():
+                counts[k] += count * v
+        else:
+            counts[element] += count
+    return counts
