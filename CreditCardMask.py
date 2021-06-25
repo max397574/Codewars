d2919db143cbaaf3766d70ede81220c990c975d1
max397574 <max.heinzer397@gmx.ch>
2021-06-25 10:44:14 +0200

    added challenge

---
+++ CreditCardMask.py
@@ -0,0 +1,10 @@
+#https://www.codewars.com/kata/5412509bd436bd33920011bc/train/python
+def maskify(cc):
+    if (len(cc)>4):
+        result = ''
+        for i in range(len(cc)-4):
+            result+='#'
+        result+=(cc[-4:])
+    else:
+        result=cc
+    return result
