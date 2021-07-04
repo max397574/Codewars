8a467b8cd3a473c34559d7d095b930d071189f18
max397574 <max.heinzer397@gmx.ch>
2021-07-04 09:30:22 +0200

    added challenge

---
+++ YesOrNo.java
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/53369039d7ab3ac506000467/train/java
+class YesOrNo{
+    public static String boolToWord(boolean b) {
+	if(b) {
+	    return "Yes";
+	}
+	else {
+	return "No";
+	}
+    }
+}
