bccd2581b71d5fb121c2f4f0de0a7eda029bd863
max397574 <max.heinzer397@gmx.ch>
2021-07-04 10:40:07 +0200

    added challenge

---
+++ WhichQuarter.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/python
+def quarter_of(month):
+    if month<=3:
+        return 1
+    elif month<=6:
+        return 2
+    elif month<=9:
+        return 3
+    else:
+        return 4
