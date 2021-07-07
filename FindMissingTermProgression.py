bb4a2ae8a11b1dad72cd12202678c10dccfc260b
max397574 <max.heinzer397@gmx.ch>
2021-07-07 17:07:18 +0200

    added challenge

---
+++ FindMissingTermProgression.py
@@ -0,0 +1,6 @@
+#https://www.codewars.com/katadef find_missing(sequence:list):
+def find_missing(sequence:list):
+    step=int((sequence[len(sequence)-1]-sequence[0])/len(sequence))
+    for k in range(1,len(sequence)):
+        if sequence[k]-sequence[k-1]!=step:
+            return(sequence[k-1]+step)
