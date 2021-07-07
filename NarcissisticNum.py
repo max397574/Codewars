d8ecfd09f2aef982f75b5c4bb98054d8f6e71ea1
max397574 <max.heinzer397@gmx.ch>
2021-07-07 10:02:17 +0200

    added challenge

---
+++ NarcissisticNum.py
@@ -0,0 +1,10 @@
+#https://www.codewars.com/kata/5287e858c6b5a9678200083c/train/python
+def narcissistic(value):
+    str_value = str(value)
+    total = 0
+    for num in str_value:
+        total+=pow(int(num),len(str_value))
+    if total == value:
+        return True
+    else:
+        return False
