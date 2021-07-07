662266263780cb24b6328d5cd0870ff36a656517
max397574 <max.heinzer397@gmx.ch>
2021-07-07 13:56:58 +0200

    added challenge

---
+++ HumanRedableDuration.py
@@ -0,0 +1,37 @@
+#https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/python
+def format_duration(seconds):
+    if seconds==0:
+        return "now"
+    time_dictionary = {
+        'year': 0,
+        'day' : 0,
+        'hour' : 0,
+        'minute' : 0,
+        'second' : 0,
+    }
+    time_durations = {
+        'year' : 365*24*60*60,
+        'day' : 24*60*60,
+        'hour' : 60*60,
+        'minute' : 60,
+        'second' : 1,
+    }
+    remaining=seconds
+    for duration in ['year', 'day', 'hour', 'minute', 'second']:
+        amount = remaining // time_durations[duration]
+        if amount == 0:
+            continue
+        time_dictionary[duration] = amount
+        remaining -= amount * time_durations[duration]
+    res=[]
+
+    for duration in ['year', 'day', 'hour', 'minute', 'second']:
+        if time_dictionary[duration]!=0:
+            if time_dictionary[duration] > 1:
+                res.append(f'{time_dictionary[duration]} {duration}s')
+            else:
+                res.append(f'{time_dictionary[duration]} {duration}')
+    if len(res) > 1:
+        return (", ".join(res[:-1]) + ' and ' + res[-1])
+    else:
+        return res[0]
