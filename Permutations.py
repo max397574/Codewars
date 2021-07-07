166e07a9fab98db98a2212b993a2b1712bcc4b47
max397574 <max.heinzer397@gmx.ch>
2021-07-07 15:04:28 +0200

    added parameter descriptions

--- Permutations.py
+++ Permutations.py
@@ -1,5 +1,5 @@
 #https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/python
-def permutations(string):
+def permutations(string: str)->list:
     result = set([string])
     if len(string) == 2:
         result.add(string[1] + string[0])
@@ -8,3 +8,4 @@ def permutations(string):
             for s in permutations(string[:i] + string[i + 1:]):
                 result.add(c + s)
     return list(result)
+print(permutations('ABC'))
