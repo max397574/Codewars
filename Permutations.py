b428d81e5dff040b88b4d4dcde6a8db1baf2943c
max397574 <max.heinzer397@gmx.ch>
2021-07-07 12:29:35 +0200

    added challenge

---
+++ Permutations.py
@@ -0,0 +1,10 @@
+#https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/python
+def permutations(string):
+    result = set([string])
+    if len(string) == 2:
+        result.add(string[1] + string[0])
+    elif len(string) > 2:
+        for i, c in enumerate(string):
+            for s in permutations(string[:i] + string[i + 1:]):
+                result.add(c + s)
+    return list(result)
