978b5c96d0bc2521c2c913e5e109ca6d03ed91b7
max397574 <max.heinzer397@gmx.ch>
2021-07-08 11:53:32 +0200

    added challenge

---
+++ SimpleMultiplication.c
@@ -0,0 +1,9 @@
+/**https://www.codewars.com/kata/583710ccaa6717322c000105/train/c*/
+int simple_multiplication(int number) {
+    if (number%2==0) {
+	return number*8;
+    }
+    else {
+	return number*9;
+    }
+}
