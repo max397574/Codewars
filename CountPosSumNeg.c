c0f4291d48ae16afcacd1c2415d0cc8a4eadda43
max397574 <max.heinzer397@gmx.ch>
2021-07-08 10:32:24 +0200

    added challenge

---
+++ CountPosSumNeg.c
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/c//
+#include <stddef.h>
+
+void count_positives_sum_negatives(
+  int *values, size_t count, int *positivesCount, int *negativesSum) 
+{
+  // Please store the positives count in the memory, pointed to
+  // by the positivesCount parameter.
+  
+  // Please store the negatives sum in the memory, pointed to
+  // by the negativesSum parameter.
+}
