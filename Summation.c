f583c907e552a9812d4542fc23a9a0a493f6dbf8
max397574 <max.heinzer397@gmx.ch>
2021-07-08 10:29:23 +0200

    added challenge

---
+++ Summation.c
@@ -0,0 +1,8 @@
+/*https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/c*/
+int summation(int num) {
+    int sum = 0;
+    for (int i = 0; i <= num; i++) {
+	sum += i;
+    }
+    return sum;
+}
