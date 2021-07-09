6c46d447e7b145c535ded6ec77fe06e56e42adca
max397574 <max.heinzer397@gmx.ch>
2021-07-09 12:03:30 +0200

    added challenge

---
+++ EvenOrOdd.c
@@ -0,0 +1,9 @@
+/**https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/c*/
+const char * even_or_odd(int number) {
+    if (number%2==0) {
+	return "Even";
+    }
+    else {
+	return "Odd";
+    }
+}
