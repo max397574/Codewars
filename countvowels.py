00d7727acae01cd5bff53027205f67579bc417ba
max397574 <max.heinzer397@gmx.ch>
2021-07-10 14:05:39 +0200

    changed formatting

--- countvowels.py
+++ countvowels.py
@@ -1,11 +1,11 @@
 # https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/python
+def countX(x,arr):
+    count=0
+    for i in arr:
+        if i==x:
+            count+=1
+    return count
 def get_count(input_str):
-    def countX(x,arr):
-        count=0
-        for i in arr:
-            if i==x:
-                count+=1
-        return count
                 
     num_vowels = 0
     num_vowels=num_vowels+countX('a',input_str)
