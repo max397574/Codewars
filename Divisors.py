f4cd45de62901d390e10b6aea17337428bee71f7
max397574 <max.heinzer397@gmx.ch>
2021-07-13 10:06:44 +0200

    added challenge

---
+++ Divisors.py
@@ -0,0 +1,10 @@
+#https://www.codewars.com/kata/544aed4c4a30184e960010f4/train/python
+def divisors(integer):
+    divisor=[]
+    for i in range(2,integer-1):
+        if integer%i==0:
+            divisor.append(i)
+    if len(divisor)==0:
+        return f'{integer} is prime'
+    else:
+        return divisor
